80f5b43eb7933d25d52f5a16af164042208c482c
Alarksharma15 <139035589+Alarksharma15@users.noreply.github.com>
2025-12-14 13:14:07 +0530

    feat: Add DeepfakeFusionModel to fuse acoustic, semantic, and signal features for deepfake detection. still more classes to add and codes to complete

---
+++ ml_pipeline_deepfake/models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Models Module
+
+Contains the fusion classifier architecture.
+"""
+
+from .fusion_model import DeepfakeFusionModel, SimpleFusionModel
+
+__all__ = [
+    'DeepfakeFusionModel',
+    'SimpleFusionModel'
+]
